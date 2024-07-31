6247c3ca4764bd236e094a87f344e1a57de2f6d3
bendzamba <bendzamba@gmail.com>
2024-07-31 14:22:19 -0400

    cleaning up formatting for SQL. adding bookshelves_books table to house relationships

--- app/db/sqllite.py
+++ app/db/sqllite.py
@@ -7,8 +7,33 @@ class DB:
         self.cursor = self.connection.cursor()
         # self.cursor.execute('''DROP TABLE IF EXISTS bookshelves''')
         # self.cursor.execute('''DROP TABLE IF EXISTS books''')
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS bookshelves (id INTEGER PRIMARY KEY, title TEXT, description TEXT)''')
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, bookshelf_id INTEGER, category TEXT, cover_image TEXT, FOREIGN KEY(bookshelf_id) REFERENCES bookshelves(id))''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS bookshelves (
+                id INTEGER PRIMARY KEY, 
+                title TEXT, 
+                description TEXT
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS books (
+                id INTEGER PRIMARY KEY, 
+                title TEXT, 
+                author TEXT, 
+                year INTEGER, 
+                bookshelf_id INTEGER, 
+                category TEXT, 
+                cover_image TEXT
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS bookshelves_books (
+                bookshelf_id INTEGER,
+                book_id INTEGER,
+                PRIMARY KEY (bookshelf_id, book_id),
+                FOREIGN KEY (bookshelf_id) REFERENCES bookshelves (id),
+                FOREIGN KEY (book_id) REFERENCES books (id)
+            )
+        ''')
         self.connection.commit()
     
     def execute(self, query: str, values: tuple = ()):
