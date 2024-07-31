bac88713717677a1d6783d80afda47701a7e6036
bendzamba <bendzamba@gmail.com>
2024-07-31 16:30:04 -0400

    adding PATCH methods for bookshelves and books.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Annotated
-from app.models.bookshelf import Bookshelf
-from app.models.book import Book, BookId
+from app.models.bookshelf import Bookshelf, BookshelfUpdate
+from app.models.book import Book, BookId, BookUpdate
 from app.services.openlibrary import OpenLibrary
 from fastapi import FastAPI, Path
 from app.db.sqllite import DB
@@ -26,6 +26,25 @@ def get_bookshelf(
     bookshelf_id: Annotated[int, Path(title="The ID of the bookshelf to get")]
 ):
     return db.execute(query='SELECT * FROM bookshelves WHERE id = ?', values=(bookshelf_id,)).fetchone()
+
+@app.patch("/bookshelves/{bookshelf_id}")
+def update_bookshelf(
+    bookshelf_id: Annotated[int, Path(title="The ID of the bookshelf to update")],
+    bookshelf: BookshelfUpdate
+):
+    update_fields = []
+    parameters = []
+    
+    for attribute in BookshelfUpdate.__fields__.keys():
+        bookshelf_dict = bookshelf.__dict__
+        if bookshelf_dict[attribute] is not None:
+            update_fields.append(f"{attribute} = ?")
+            parameters.append(bookshelf_dict[attribute])
+
+    query = f"UPDATE bookshelves SET {', '.join(update_fields)} WHERE id = ?"
+    parameters.append(bookshelf_id)
+    db.execute(query=query, values=parameters)
+    return {"message": f"Bookshelf id {bookshelf_id} has been updated"}
 
 @app.get("/bookshelves/{bookshelf_id}/books")
 def get_bookshelf_books(
@@ -67,3 +86,22 @@ async def create_book(
     cursor = db.execute(query='INSERT INTO books (title, author, year, category, cover_image) VALUES (?, ?, ?, ?, ?)', values=(book.title, book.author, book.year, book.category, cover_image))
     book_id = cursor.lastrowid
     return {"message": f"New book id: {book_id}"}
+
+@app.patch("/books/{book_id}")
+def update_bookshelf(
+    book_id: Annotated[int, Path(title="The ID of the book to update")],
+    book: BookUpdate
+):
+    update_fields = []
+    parameters = []
+    
+    for attribute in BookUpdate.__fields__.keys():
+        book_dict = book.__dict__
+        if book_dict[attribute] is not None:
+            update_fields.append(f"{attribute} = ?")
+            parameters.append(book_dict[attribute])
+
+    query = f"UPDATE books SET {', '.join(update_fields)} WHERE id = ?"
+    parameters.append(book_id)
+    db.execute(query=query, values=parameters)
+    return {"message": f"Book id {book_id} has been updated"}
