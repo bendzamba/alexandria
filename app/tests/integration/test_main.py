5d1b88c958f8bbcecf10cbd2183861c35bc4caed
bendzamba <bendzamba@gmail.com>
2024-07-31 00:35:16 -0400

    adding integration tests for book routes

--- app/tests/integration/test_main.py
+++ app/tests/integration/test_main.py
@@ -15,6 +15,9 @@ def test_create_bookshelf():
     assert response.status_code == 200
     assert response.json() == {"create_bookshelf": "success"}
 
+    response = client.post("/bookshelf")
+    assert response.status_code == 422
+
 def test_get_bookshelf():
     bookshelf_id = 1
     response = client.get(f"/bookshelf/{bookshelf_id}")
@@ -26,3 +29,23 @@ def test_get_bookshelf_books():
     response = client.get(f"/bookshelf/{bookshelf_id}/books")
     assert response.status_code == 200
     assert response.json() == {"get_bookshelf_books "  + str(bookshelf_id): "success"}
+
+def test_get_books():
+    response = client.get("/books")
+    assert response.status_code == 200
+    assert response.json() == {"get_books": "success"}
+
+def test_create_book():
+    body = {'title': 'This is a title', 'author': 'John Doe', 'year': 2000, 'category': 'Biography'}
+    response = client.post("/book", json=body)
+    assert response.status_code == 200
+    assert response.json() == {"create_book": "success"}
+
+    response = client.post("/book")
+    assert response.status_code == 422
+
+def test_get_book():
+    book_id = 1
+    response = client.get(f"/book/{book_id}")
+    assert response.status_code == 200
+    assert response.json() == {"get_book " + str(book_id): "success"}
