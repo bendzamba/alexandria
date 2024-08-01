bb76decef4cdc2223ab503ada1b424be0aeafa21
bendzamba <bendzamba@gmail.com>
2024-08-01 18:14:13 -0400

    updating main App component to render Bookshelves children

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
+import Bookshelves from './components/Bookshelves/Bookshelves';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1 class="display-2">Welcome to your Book Case</h1>
+        </Col>
+      </Row>
+      <Bookshelves></Bookshelves>
+    </Container>
   );
 }
 
