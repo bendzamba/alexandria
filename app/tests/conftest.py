c8a01cf414b68b36b13a3470b8941bf899e4f57f
bendzamba <bendzamba@gmail.com>
2024-08-01 12:49:02 -0400

    updating tests. adding conftest.py which mocks our DB in memory, as well as openlibrary, removing the live HTTPX call to their API and stubbing results. this file is picked up automatically by pytest and files that import db and openlibrary will receive these mocked versions. integration tests for these routes are almost end-to-end test-ish, in that we are mocking the DB and creating resources and then getting, patching, deleting said resources. throughout the course of the test_routes.py, the order of the tests matters, as they are run one at a time, and one will create a resource while the following will check that the resource exists. there may be a better way to handle this, but for now I'm happy that it provides fairly full coverage for the CRUD operations

---
+++ app/tests/conftest.py
@@ -0,0 +1,77 @@
+import sys
+import sqlite3
+
+class DB:
+
+    def __init__(self):
+        self.connection = sqlite3.connect(':memory:', check_same_thread=False)
+        self.connection.row_factory = sqlite3.Row
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("PRAGMA foreign_keys = ON")
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS bookshelves (
+                id INTEGER PRIMARY KEY, 
+                title TEXT, 
+                description TEXT
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS books (
+                id INTEGER PRIMARY KEY, 
+                title TEXT, 
+                author TEXT, 
+                year INTEGER, 
+                category TEXT, 
+                cover_image TEXT
+            )
+        ''')
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS bookshelves_books (
+                bookshelf_id INTEGER,
+                book_id INTEGER,
+                PRIMARY KEY (bookshelf_id, book_id),
+                FOREIGN KEY (bookshelf_id) REFERENCES bookshelves (id) ON DELETE CASCADE,
+                FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE
+            )
+        ''')
+        self.connection.commit()
+    
+    def execute(self, query: str, values: tuple = ()):
+        self.cursor.execute(query, values)
+        self.connection.commit()
+        return self.cursor
+    
+db_instance = None
+
+def get_db():
+    global db_instance
+    if db_instance is None:
+        db_instance = DB()
+    try:
+        yield db_instance
+    finally:
+        pass
+
+# Mock our system module so that downstream files that import app.db.sqlite will use our mock
+module = type(sys)('app.db.sqlite')
+module.get_db = get_db
+sys.modules['app.db.sqlite'] = module
+
+
+from app.models.book import Book
+from typing import Any, Dict
+
+class OpenLibrary:
+
+    def __init__(self):
+        self.search_url = "https://openlibrary.org/search.json?title={title}"
+
+    async def search(self, book: Book) -> Dict[str, Any]:
+        return {}
+        
+    def find_olid(self, olid_response: Dict[str, Any]) -> str | None:   
+        return '12345'
+
+module = type(sys)('app.services.openlibrary')
+module.OpenLibrary = OpenLibrary
+sys.modules['app.services.openlibrary'] = module
