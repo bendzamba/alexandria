d173879eb1cd5454abbc7e287602906762db64b8
bendzamba <bendzamba@gmail.com>
2024-08-02 13:21:06 -0400

    adding additional route for viewing a single bookshelf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Bookshelves from './components/bookshelves/Bookshelves';
+import Bookshelf from './components/bookshelves/Bookshelf';
 import CreateBookshelf from './components/bookshelves/CreateBookshelf';
 import UpdateBookshelf from './components/bookshelves/UpdateBookshelf';
 import Header from './components/common/Header';
@@ -14,6 +15,7 @@ function App() {
         <div className="container mt-4">
           <Routes>
             <Route path="/" element={<Bookshelves />} />
+            <Route path="/bookshelves/:id" element={<Bookshelf />} />
             <Route path="/bookshelves/create" element={<CreateBookshelf />} />
             <Route path="/bookshelves/update/:id" element={<UpdateBookshelf />} />
           </Routes>
