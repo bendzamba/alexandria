575efaa9e81beef9af75751f9cf891a5490501ac
bendzamba <bendzamba@gmail.com>
2024-08-02 13:25:17 -0400

    fixing bookshelves' service api calls. returning json only if there is an expected body.

--- frontend/src/services/bookshelves.js
+++ frontend/src/services/bookshelves.js
@@ -6,14 +6,13 @@ export const GetBookshelves = async () => {
 };
 
 export const CreateBookshelf = async (data) => {
-  const response = await fetch(`${API_BASE_URL}/bookshelves/`, {
+  return await fetch(`${API_BASE_URL}/bookshelves/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
-  return await response.json();
 }
 
 export const GetBookshelf = async (id) => {
@@ -22,18 +21,17 @@ export const GetBookshelf = async (id) => {
 };
 
 export const UpdateBookshelf = async (id, data) => {
-  const response = await fetch(`${API_BASE_URL}/bookshelves/${id}/`, {
+  return await fetch(`${API_BASE_URL}/bookshelves/${id}`, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   });
-  return await response.json();
 }
 
 export const DeleteBookshelf = async (id) => {
-  await fetch(`${API_BASE_URL}/bookshelves/${id}/`, {
+  return await fetch(`${API_BASE_URL}/bookshelves/${id}/`, {
     method: 'DELETE',
   });
 }
@@ -44,18 +42,17 @@ export const GetBookshelfBooks = async (id) => {
   };
 
 export const AddBookToBookshelf = async (bookshelfId, bookId) => {
-  const response = await fetch(`${API_BASE_URL}/bookshelves/${bookshelfId}/books`, {
+  return await fetch(`${API_BASE_URL}/bookshelves/${bookshelfId}/books`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ 'book_id': bookId }),
   });
-  return await response.json();
 };
 
 export const DeleteBookFromBookshelf = async (bookshelfId, bookId) => {
-  await fetch(`${API_BASE_URL}/bookshelves/${bookshelfId}/books/${bookId}`, {
+  return await fetch(`${API_BASE_URL}/bookshelves/${bookshelfId}/books/${bookId}`, {
     method: 'DELETE',
   });
 }
