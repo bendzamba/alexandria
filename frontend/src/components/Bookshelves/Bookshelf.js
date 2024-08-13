5a440c3007e0671f90319799d3d04ec9e316655d
bendzamba <bendzamba@gmail.com>
2024-08-13 12:39:44 -0400

    using buttons with onclick behavior instead of navlink. namely, want to be able to perform some business logic before navigation, especially in the case of deletion.

--- frontend/src/components/Bookshelves/Bookshelf.js
+++ frontend/src/components/Bookshelves/Bookshelf.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import { NavLink, useParams } from 'react-router-dom';
+import { NavLink, useParams, useNavigate } from 'react-router-dom';
 import { GetBookshelfBooks, GetBookshelf } from '../../services/bookshelves'
 
 function Bookshelf({ bookshelfId = null, preview = false }) {
@@ -13,6 +13,7 @@ function Bookshelf({ bookshelfId = null, preview = false }) {
   const [data, setData] = useState([]);
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,10 +46,20 @@ function Bookshelf({ bookshelfId = null, preview = false }) {
     return <div>Loading...</div>;
   }
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    navigate(`/bookshelves/update/` + id);
+  };
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    // navigate(`/bookshelves/update/{id}`);
+  };
+
   return (
     <Container>
-        <Row className="mt-4 align-items-center" style={{ 'borderBottom': '3px solid black' }}>
-            <Col md="auto">
+        <Row className="mt-4 align-items-center" style={{ 'borderBottom': '3px solid black'}}>
+            <Col xs={9}>
               <NavLink 
                 className="nav-link" 
                 to={"/bookshelves/" + id}
@@ -56,23 +67,25 @@ function Bookshelf({ bookshelfId = null, preview = false }) {
                   <span className="h4 pull-left">{data.title}</span>
               </NavLink>
             </Col>
-            <Col md="auto" style={{
-              textAlign:"right"
-            }}>
-              <span className="text-secondary">{data.description}</span>
-            </Col>
             { ! preview && (
-              <Col md="auto" style={{
+              <Col xs={3} style={{
                 textAlign:"right"
               }}>
-                <NavLink 
+                <button type="button" className="btn btn-primary" onClick={handleUpdate}>Update</button>
+                <button type="button" className="btn btn-danger ms-1" onClick={handleDelete}>Delete</button>
+                {/* <NavLink 
                   className="nav-link" 
                   to={"/bookshelves/update/" + id}
                   >
                     Update Bookshelf
-                </NavLink>
+                </NavLink> */}
               </Col>
             )}
+        </Row>
+        <Row>
+          <Col md="auto">
+              <span className="text-secondary">{data.description}</span>
+          </Col>
         </Row>
         <Row className="mt-2">
             {books.slice(0, 5).map((book) => (
