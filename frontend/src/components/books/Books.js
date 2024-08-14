26f610ba0a79f8b9fb201f836c40cfc9812d5f32
bendzamba <bendzamba@gmail.com>
2024-08-14 17:05:50 -0400

    updating how Book and Books are handled, similar to Bookshelves, where a Book component is responsible for fetching its own data. again, possibly overkill, in that the Books component does fetch everything, but if we navigate directly to a Book without coming through the Books page, we wouldn't have any data passed from Books. trade off. possibly could wrap all of it (and bookshelf logic) in conditions

--- frontend/src/components/books/Books.js
+++ frontend/src/components/books/Books.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import { NavLink } from 'react-router-dom';
 import Book from './Book';
 import { GetBooks } from '../../services/books'
 
@@ -35,12 +34,8 @@ function Books() {
         {books.map((book) => (
           <Col xs={4} className="mt-3 mb-3">
             <Book
-              id={book.id}
-              title={book.title}
-              author={book.author}
-              year={book.year}
-              category={book.category}
-              cover={book.cover_image}
+              bookId={book.id}
+              preview={true}
             />
           </Col>
         ))}
