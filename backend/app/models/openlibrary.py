11a0ce89fc659752482d3f2be212c84c1524391e
bendzamba <bendzamba@gmail.com>
2024-08-18 11:18:07 -0400

    The Open Library API may be inconsistent with their response schema and optional/required keys. Here we make the cover image keys optional, and then combine them into a single list for response from our API. if the cover edition does not exist in  'all editions', we add it. anecdotally, it seems to typically exist in that list, and is simply the 'default' image they've selected

--- backend/app/models/openlibrary.py
+++ backend/app/models/openlibrary.py
@@ -1,14 +1,26 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Work(BaseModel):
     title: str
     author_name: list[str]
     first_publish_year: int
-    cover_edition_key: str
-    edition_key: list[str]
+    # If there are no covers we can use a default image
+    cover_edition_key: Optional[str] = None
+    edition_key: Optional[list[str]] = []
 
     def model_dump(self, **kwargs):
         data = super().model_dump(**kwargs)
         # Replace the 'author' field with the first author
         data['author_name'] = self.author_name[0]
+        if self.cover_edition_key is not None:
+            if self.cover_edition_key in self.edition_key:
+                self.edition_key.insert(0, self.edition_key.pop(self.edition_key.index(self.cover_edition_key)))
+                data['edition_keys'] = self.edition_key
+            else:
+                data['edition_keys'] = [self.cover_edition_key] + self.edition_key
+        else:
+            data['edition_keys'] = self.edition_key
+        del data['cover_edition_key']
+        del data['edition_key']
         return data
