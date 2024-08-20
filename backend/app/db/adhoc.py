5760c3ef42d6a2973eb254afd1cf1e8e543ccd21
bendzamba <bendzamba@gmail.com>
2024-08-20 00:17:38 -0400

    adding rating and review fields to the database, updating our current db to include the fields

--- backend/app/db/adhoc.py
+++ backend/app/db/adhoc.py
@@ -43,8 +43,23 @@ def rename_cover_olid_to_olid_in_books_table():
     cursor.execute('ALTER TABLE books RENAME cover_olid TO olid;')
     connection.commit()
 
+def add_rating_and_review_to_books_table():
+    connection = sqlite3.connect('backend/app/bookshelf.db', check_same_thread=False)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute('''
+        ALTER TABLE books
+        ADD COLUMN rating INT;
+    ''')
+    cursor.execute('''
+        ALTER TABLE books
+        ADD COLUMN review TEXT;
+    ''')
+    connection.commit()
+
 # rename_cover_image_to_cover_uri_in_books_table()
 # add_cover_olid_to_books_table()
 # show_table_schema('books')
 # remove_column_from_table('bookshelf_id', 'books')
-rename_cover_olid_to_olid_in_books_table()
+# rename_cover_olid_to_olid_in_books_table()
+add_rating_and_review_to_books_table()
