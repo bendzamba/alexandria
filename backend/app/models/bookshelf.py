cf841d514f9e4bae2de67dcc2800c9319ed3213a
bendzamba <bendzamba@gmail.com>
2024-08-21 23:12:11 -0400

    updating our book and bookshelf models, and adding a relationship model. here we are altering our definitions to make them valid for sql model, which in turn is valid for pydantic and sqlalchemy. the base classes contain the similar fields that need inheriting in a few places. the classes indicating 'table=true' actually define the DB schema. the other classes are for response formats, and any additional rules around pydantic validating creation and update. the TYPE_CHECKING and late imports + model_rebuild() calls are all to do with not running afoul of python's linting/type checking, and circular import error at runtime

--- backend/app/models/bookshelf.py
+++ backend/app/models/bookshelf.py
@@ -1,11 +1,32 @@
-from pydantic import BaseModel, create_model
-from typing import Optional
+from sqlmodel import SQLModel, Field, Relationship
+from pydantic import create_model
+from typing import Optional, TYPE_CHECKING, List
+from app.models.book_bookshelf import BookBookshelfLink
 
-class Bookshelf(BaseModel):
+if TYPE_CHECKING:
+    from app.models.book import Book, BookPublic  # Only imported when type checking
+
+class BookshelfBase(SQLModel):
     title: str
     description: str
 
+class Bookshelf(BookshelfBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    books: List["Book"] = Relationship(back_populates="bookshelves", link_model=BookBookshelfLink, sa_relationship_kwargs=dict(lazy="selectin"))
+
+class BookshelfCreate(BookshelfBase):
+    pass
+
+class BookshelfPublic(BookshelfBase):
+    id: int
+
 BookshelfUpdate = create_model(
     'BookshelfUpdate',
-    **{field: (Optional[type_hint], None) for field, type_hint in Bookshelf.__annotations__.items()}
+    **{field: (Optional[type_hint], None) for field, type_hint in BookshelfBase.__annotations__.items()}
 )
+
+class BookshelfPublicWithBooks(BookshelfPublic):
+    books: list["BookPublic"] = []
+
+from app.models.book import BookPublic
+BookshelfPublicWithBooks.model_rebuild()
