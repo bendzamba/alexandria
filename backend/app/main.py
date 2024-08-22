976665bfc34995627eb572cf77f0b686adfa3b0b
bendzamba <bendzamba@gmail.com>
2024-08-21 20:18:47 -0400

    beginning to swap out raw sql and sqlite3 package for SQL Model which gives us the ORM capability of SQL Alchemy, and the validation of Pydantic in one. in our main file, we create all tables (if they dont exist) using our schemas defined in our models for Book and Bookshelf

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI, Path
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.routers import books, bookshelves
-import sqlite3
+from sqlmodel import SQLModel, text
+from app.models.book import Book # imported for create_all call
+from app.models.bookshelf import Bookshelf # imported for create_all call
+from app.db.sqlite import DB
 
-app = FastAPI()
+db = DB()
+engine = db.get_engine()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    with engine.connect() as connection:
+        connection.execute(text("PRAGMA foreign_keys=ON"))  # for SQLite only
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 origins = ["*"]
 
@@ -18,41 +32,3 @@ app.add_middleware(
 
 app.include_router(books.router, prefix="/books", tags=["books"])
 app.include_router(bookshelves.router, prefix="/bookshelves", tags=["bookshelves"])
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    connection = sqlite3.connect('bookshelf.db', check_same_thread=False)
-    cursor = connection.cursor()
-    # self.cursor.execute('''DROP TABLE IF EXISTS bookshelves''')
-    # self.cursor.execute('''DROP TABLE IF EXISTS books''')
-    # self.cursor.execute('''DROP TABLE IF EXISTS bookshelves_books''')
-    cursor.execute("PRAGMA foreign_keys = ON")
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS bookshelves (
-            id INTEGER PRIMARY KEY, 
-            title TEXT, 
-            description TEXT
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS books (
-            id INTEGER PRIMARY KEY, 
-            title TEXT, 
-            author TEXT, 
-            year INTEGER, 
-            olid TEXT,
-            cover_uri TEXT,
-            rating INTEGER
-            review TEXT
-        )
-    ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS bookshelves_books (
-            bookshelf_id INTEGER,
-            book_id INTEGER,
-            PRIMARY KEY (bookshelf_id, book_id),
-            FOREIGN KEY (bookshelf_id) REFERENCES bookshelves (id) ON DELETE CASCADE,
-            FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE
-        )
-    ''')
-    connection.commit()
