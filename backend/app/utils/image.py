be275a085cfc5f94566c3ef991608c33411e92cc
bendzamba <bendzamba@gmail.com>
2024-09-04 15:50:12 -0400

    changing how we handle image directory. need path and name separately. first for checking if file exists, second for saving path to database. this is still messy generally of course

--- backend/app/utils/image.py
+++ backend/app/utils/image.py
@@ -8,10 +8,11 @@ load_dotenv()
 
 class Image:
     def __init__(self):
-        self.local_image_directory = os.getenv("IMAGES_DIRECTORY")
+        self.local_image_directory_path = os.getenv("IMAGES_DIRECTORY_PATH")
+        self.local_image_directory_name = os.getenv("IMAGES_DIRECTORY_NAME")
         self.default_cover_image = "No_Image_Available.jpg"
         self.relative_path_to_file = (
-            os.getenv("IMAGES_DIRECTORY") + "/" + "{filename}{extension}"
+            self.local_image_directory_path + self.local_image_directory_name + "/" + "{filename}{extension}"
         )
         self.image_file_extension = ".jpg"
 
@@ -32,11 +33,8 @@ class Image:
     async def download(self, remote_url: str, local_filename: str) -> None:
         return urllib.request.urlretrieve(remote_url, local_filename)
 
-    def get_local_image_directory(self) -> str:
-        return self.local_image_directory
-
     def get_default_cover_with_path_for_database(self) -> str:
-        return self.local_image_directory + self.default_cover_image
+        return "/" + self.local_image_directory_name + "/" + self.default_cover_image
 
     def get_cover_with_path_for_database(self, filename: str) -> str:
-        return self.local_image_directory + filename + self.image_file_extension
+        return "/" + self.local_image_directory_name + "/" + filename + self.image_file_extension
