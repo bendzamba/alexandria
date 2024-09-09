b316e5194ab994519014a62aacf4b03b7f540999
bendzamba <bendzamba@gmail.com>
2024-09-08 22:56:14 -0400

    of course there's too much in this commit, but here we go. in attempting to sort by author's last name, i needed to create a sorting function type, that could be easily applied to multiple scenarios. to avoid conditional logic in the .sort() itself, i pass all values to a mapper. if we've defined a sorting function for that value, then we use it, if not we just return the value. the sorting functions themselves sometimes require the full Book in question, and sometimes do not. I realized this when trying to also add some sorting for reading dates, because I reasoned that we should sort by 'end date', meaning the day they finished the book. but if there is a start date and no end date, that means they are still reading it... so the 'end' should be 'today' so as to order it correctly in time. so this necessitated access to the Book proper. had to work in some generics for easier type declaration. also made a subset of 'sortable' columns as an interface to use, as not all columns will be sortable naturally.

--- frontend/src/components/books/Books.tsx
+++ frontend/src/components/books/Books.tsx
@@ -4,7 +4,10 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Book from "./Book";
 import { GetBooks } from "../../services/books";
-import { BookWithBookshelvesInterface } from "../../interfaces/book_and_bookshelf";
+import {
+  BookWithBookshelvesInterface,
+  SortableBookProperties,
+} from "../../interfaces/book_and_bookshelf";
 import styles from "./css/Books.module.scss";
 
 function Books() {
@@ -17,6 +20,10 @@ function Books() {
     () => localStorage.getItem("sortDirection") || "ascending"
   );
   const [loading, setLoading] = useState(true);
+  type SortFunction<T> = {
+    (value: T, book: BookWithBookshelvesInterface): string | Date | null;
+    (value: T): string | Date | null;
+  };
 
   useEffect(() => {
     localStorage.setItem("sort", sort);
@@ -59,6 +66,97 @@ function Books() {
     );
   };
 
+  const surnameSort: SortFunction<string> = (author_name: string) => {
+    /* 
+    There may be a more intelligent way of handling this, such as procuring
+    proper, semantic `first` and `last` names for each Author. In lieu of
+    that, we attempt to determine whether or not their `double-barrelled` surname
+    should be preserved for sorting.
+    Example:
+    Ursula K. Le Guin should be sorted by L, not G
+    Guy de Maupassant should be sorted by M, not D
+    */
+
+    const nameParts = author_name.split(" ");
+
+    // List of integral prefixes (part of surname)
+    const integralPrefixes = ["le", "la", "de la"];
+
+    // List of common surname prefixes to ignore
+    const ignorePrefixes = [
+      "van",
+      "von",
+      "de",
+      "del",
+      "di",
+      "da",
+      "al",
+      "bin",
+      "mc",
+      "mac",
+      "ibn",
+    ];
+
+    // Start by assuming the last name is the surname
+    let surname = nameParts[nameParts.length - 1];
+
+    // Check if the name includes an integral prefix
+    for (let i = nameParts.length - 1; i > 0; i--) {
+      const potentialPrefix = nameParts[i - 1].toLowerCase();
+      const fullSurnameCandidate = nameParts.slice(i - 1).join(" ");
+
+      // If the prefix is part of the surname (e.g., "Le Guin")
+      if (integralPrefixes.includes(potentialPrefix)) {
+        surname = fullSurnameCandidate;
+        break;
+      }
+
+      // If we find an ignorable prefix, we stop and return the next part as surname
+      else if (ignorePrefixes.includes(potentialPrefix)) {
+        surname = nameParts[i];
+        break;
+      }
+    }
+
+    return surname.toLowerCase();
+  };
+
+  const dateSort: SortFunction<string> = (
+    read_end_date_string: string | null,
+    book?: BookWithBookshelvesInterface
+  ) => {
+    if (book === undefined) {
+      console.log("Can't sort by date as book is undefined");
+      return null;
+    }
+    // If there is a start date, but no end date, the end
+    // should be "today" for proper sorting
+    if (!read_end_date_string) {
+      if (book.read_start_date) {
+        return new Date();
+      }
+      return null;
+    }
+    return new Date(read_end_date_string);
+  };
+
+  const sortFunctions: Record<string, SortFunction<any>> = {
+    author: surnameSort as SortFunction<string>,
+    read_end_date: dateSort as SortFunction<string>,
+  };
+
+  const mapValueToSortable = (
+    key: string,
+    value: string | number | null,
+    book: BookWithBookshelvesInterface
+  ) => {
+    // We've defined no specific sorting function for the value
+    if (!(key in sortFunctions)) {
+      return value;
+    }
+    return sortFunctions[key](value, book);
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -93,6 +191,7 @@ function Books() {
               <option value="author">Author</option>
               <option value="year">Year</option>
               <option value="rating">Rating</option>
+              <option value="read_end_date">Reading Dates</option>
             </select>
             <label htmlFor="floatingSelect">Sort by</label>
           </div>
@@ -143,30 +242,43 @@ function Books() {
               bookA: BookWithBookshelvesInterface,
               bookB: BookWithBookshelvesInterface
             ) => {
-              const sortKey = sort as keyof BookWithBookshelvesInterface;
+              const sortKey = sort as keyof SortableBookProperties;
               const directionModifier = sortDirection === "ascending" ? 1 : -1;
               if (
-                !bookA ||
-                !bookB ||
                 bookA[sortKey] === undefined ||
                 bookB[sortKey] === undefined
               ) {
                 return 0;
               }
+              // If the value is null, we essentially remove it from the
+              // order by placing it at the very end with either 0 or Infinity
+              // depending on sort direction
               let nullPosition = Infinity;
               if (directionModifier === -1) {
                 nullPosition = 0;
               }
-              const bookAComparison =
-                bookA[sortKey] !== null ? bookA[sortKey] : nullPosition;
-              const bookBComparison =
-                bookB[sortKey] !== null ? bookB[sortKey] : nullPosition;
+              const bookAMappedValue = mapValueToSortable(
+                sortKey,
+                bookA[sortKey],
+                bookA
+              );
+              const bookAComparison = bookAMappedValue
+                ? bookAMappedValue
+                : nullPosition;
+              const bookBMappedValue = mapValueToSortable(
+                sortKey,
+                bookB[sortKey],
+                bookB
+              );
+              const bookBComparison = bookBMappedValue
+                ? bookBMappedValue
+                : nullPosition;
               if (bookAComparison < bookBComparison) {
                 return -1 * directionModifier;
               } else if (bookAComparison > bookBComparison) {
                 return 1 * directionModifier;
               }
-              // a must be equal to b
+              // A and B are equal
               return 0;
             }
           )
