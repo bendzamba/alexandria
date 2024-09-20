ba4fc93515dcb95dba3f843a86d72bfc74e73799
bendzamba <bendzamba@gmail.com>
2024-09-19 21:50:34 -0400

    bucket acl not used

--- terraform/modules/frontend/s3.tf
+++ terraform/modules/frontend/s3.tf
@@ -7,11 +7,6 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
-resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-  bucket  = aws_s3_bucket.s3_bucket.bucket
-  acl     = "private"
-}
-
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.s3_bucket.id
   policy = data.aws_iam_policy_document.iam_policy_document.json
