46cabfd09f1f5bb4d4cdab49af6db33434b8c0f4
bendzamba <bendzamba@gmail.com>
2024-09-19 21:45:33 -0400

    adding mount targets, giving broad permissions, setting path to /lambda to match lambda mount

--- terraform/modules/backend/efs.tf
+++ terraform/modules/backend/efs.tf
@@ -17,11 +17,11 @@ resource "aws_efs_access_point" "efs_access_point" {
   }
 
   root_directory {
-    path = "/"
+    path = "/lambda"
     creation_info {
       owner_gid   = 1000
       owner_uid   = 1000
-      permissions = "0755"
+      permissions = "0777"
     }
   }
 
@@ -29,4 +29,16 @@ resource "aws_efs_access_point" "efs_access_point" {
     application = var.app_name
     environment = var.environment
   }
+}
+
+resource "aws_efs_mount_target" "efs_mount_target_1" {
+  file_system_id  = aws_efs_file_system.efs_file_system.id
+  subnet_id       = aws_subnet.subnet_1.id
+  security_groups = [aws_security_group.efs_sg.id]
+}
+
+resource "aws_efs_mount_target" "efs_mount_target_2" {
+  file_system_id  = aws_efs_file_system.efs_file_system.id
+  subnet_id       = aws_subnet.subnet_2.id
+  security_groups = [aws_security_group.efs_sg.id]
 }
