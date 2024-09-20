7bc4820b469aecf178ddc413996275de7655a069
bendzamba <bendzamba@gmail.com>
2024-09-19 21:47:37 -0400

    this is incomplete based on my current setup after a lot of debugging, but still adds some useful bits re: vpc and permissions

--- terraform/modules/backend/lambda.tf
+++ terraform/modules/backend/lambda.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "aws_iam_role" "lambda_exec" {
   name = "${var.app_name}-backend-iam-role-lambda-exec-${var.environment}"
 
@@ -20,29 +22,125 @@ resource "aws_iam_role" "lambda_exec" {
   }
 }
 
+resource "aws_iam_policy" "lambda_vpc_policy" {
+  name    = "${var.app_name}-backend-iam-policy-vpc-${var.environment}"
+  policy  = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "ec2:DescribeSecurityGroups"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "ec2:DescribeSubnets"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "ec2:DescribeVpcs"
+        Resource = "*"
+      },
+      {
+        Effect    = "Allow"
+        Action    = "lambda:InvokeFunction"
+        Resource  = "arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${aws_lambda_function.lambda_function.function_name}"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "elasticfilesystem:ClientMount",
+          "elasticfilesystem:ClientRootAccess",
+          "elasticfilesystem:ClientWrite",
+          "elasticfilesystem:DescribeMountTargets"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeSubnets",
+          "ec2:DeleteNetworkInterface",
+          "ec2:AssignPrivateIpAddresses",
+          "ec2:UnassignPrivateIpAddresses"
+        ],
+        Resource = "*"
+      }
+    ] 
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_vpc_policy_attachment" {
+  role        = aws_iam_role.lambda_exec.name
+  policy_arn  = aws_iam_policy.lambda_vpc_policy.arn
+}
+
 
 resource "aws_lambda_function" "lambda_function" {
   function_name = "${var.app_name}-backend-lambda-function-${var.environment}"
   s3_bucket     = "${var.app_name}-backend-s3-bucket-lambda-code-${var.environment}"
   s3_key        = "${var.app_name}.zip"
   role          = aws_iam_role.lambda_exec.arn
-  handler       = "main.handler"
+  handler       = "app.main.handler"
   runtime       = "python3.12"
   timeout       = 60
 
   environment {
     variables = {
-      DATABASE_PATH = "/mnt/efs/${var.app_name}.db"
+      DATABASE_URL = "/mnt/efs/${var.app_name}.db"
     }
   }
 
   file_system_config {
     arn               = aws_efs_access_point.efs_access_point.arn
-    local_mount_path  = "/mnt/efs"
+    local_mount_path  = "/mnt/lambda"
   }
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+
+  depends_on = [ aws_efs_mount_target.efs_mount_target_1, aws_efs_mount_target.efs_mount_target_2 ]
 
   tags = {
     application = var.app_name
     environment = var.environment
   }
+}
+
+resource "aws_lambda_permission" "apigateway_invoke_lambda" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # This gives permission to the specific API Gateway to invoke the Lambda
+  source_arn    = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.api_gateway_res_api.id}/*/ANY/${aws_api_gateway_resource.api_gateway_resource.path_part}"
 }
