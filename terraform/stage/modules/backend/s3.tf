5010627a9b5722cdd1a2ea9dead79584e0a7de96
bendzamba <bendzamba@gmail.com>
2024-10-01 13:02:07 -0400

    creating images bucket for book covers

--- terraform/stage/modules/backend/s3.tf
+++ terraform/stage/modules/backend/s3.tf
@@ -1,4 +1,4 @@
-# This file creates the S3 bucket to host our Lambda function's code
+# The following creates the S3 bucket to host our Lambda function's code
 # However, we will not use Terraform to configure our Lambda function to point
 # to this bucket, as to do so would require there to be a ZIP file present
 # and we don't want to couple this process with building, zipping and pushing
@@ -13,8 +13,27 @@ resource "aws_s3_bucket" "lambda_bucket" {
   }
 }
 
-resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+resource "aws_s3_bucket_versioning" "s3_lambda_bucket_versioning" {
   bucket = aws_s3_bucket.lambda_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+# The following creates the S3 bucket that the backend uses for storing
+# and serving static book cover image files that are downloaded from Open Library
+
+resource "aws_s3_bucket" "images_bucket" {
+  bucket  = "${var.app_name}-backend-s3-bucket-images-${var.environment}"
+  tags    = {
+    application = var.app_name
+    environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_images_bucket_versioning" {
+  bucket = aws_s3_bucket.images_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
