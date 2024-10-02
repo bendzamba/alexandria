91f9b288e4c77333f4e238b543e097fbaed76c09
bendzamba <bendzamba@gmail.com>
2024-10-02 16:39:49 -0400

    only need a mount target in private subnet

--- terraform/stage/modules/backend/efs.tf
+++ terraform/stage/modules/backend/efs.tf
@@ -32,13 +32,7 @@ resource "aws_efs_access_point" "efs_access_point" {
   }
 }
 
-resource "aws_efs_mount_target" "efs_mount_target_1" {
-  file_system_id  = aws_efs_file_system.efs_file_system.id
-  subnet_id       = aws_subnet.public_subnet.id
-  security_groups = [aws_security_group.efs_security_group.id]
-}
-
-resource "aws_efs_mount_target" "efs_mount_target_2" {
+resource "aws_efs_mount_target" "efs_mount_target" {
   file_system_id  = aws_efs_file_system.efs_file_system.id
   subnet_id       = aws_subnet.private_subnet_1.id
   security_groups = [aws_security_group.efs_security_group.id]
