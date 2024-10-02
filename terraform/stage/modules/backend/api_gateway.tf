fdab2b9dbf800ea7b80f07c829bb26883dafcfea
bendzamba <bendzamba@gmail.com>
2024-10-02 18:59:15 -0400

    this method response seems to end up being a string if I reference it the previous way. explicitly setting as an int

--- terraform/stage/modules/backend/api_gateway.tf
+++ terraform/stage/modules/backend/api_gateway.tf
@@ -182,7 +182,7 @@ resource "aws_api_gateway_integration_response" "cors_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway_rest_api.id
   resource_id = aws_api_gateway_resource.api_gateway_resource.id
   http_method = aws_api_gateway_method.api_gateway_options_method.http_method
-  status_code = aws_api_gateway_method_response.api_gateway_options_method_response.status_code
+  status_code = 200
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
