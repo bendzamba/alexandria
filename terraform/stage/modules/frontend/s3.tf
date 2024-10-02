43eca4ad18d824afc9ace730b98f1eaa8ee111b6
bendzamba <bendzamba@gmail.com>
2024-10-02 12:08:45 -0400

    adding versioning and more explicit naming

--- terraform/stage/modules/frontend/s3.tf
+++ terraform/stage/modules/frontend/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "frontend_s3_bucket" {
   bucket = "${var.app_name}-frontend-s3-bucket-${var.environment}"
 
   tags = {
@@ -7,7 +7,14 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "s3_lambda_bucket_versioning" {
+  bucket = aws_s3_bucket.frontend_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  bucket = aws_s3_bucket.s3_bucket.id
+  bucket = aws_s3_bucket.frontend_s3_bucket.id
   policy = data.aws_iam_policy_document.iam_policy_document.json
 }
