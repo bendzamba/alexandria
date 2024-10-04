f2c2352597e7b267ccc82355829673de5651c5e9
bendzamba <bendzamba@gmail.com>
2024-10-04 09:19:26 -0400

    adding terraform files to create oidc role with appropriate terraform permissions, to be used by github actions

---
+++ terraform/oidc/terraform.tfvars
@@ -0,0 +1,4 @@
+# This can be either your user name or organization name
+github_username = "bendzamba"
+github_repository = "alexandria"
+region = "us-east-1"
---
+++ terraform/oidc/variables.tf
@@ -0,0 +1,11 @@
+variable "github_username" {
+  type = string
+}
+
+variable "github_repository" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
