4db8e05f88420ca6ba3a1684ae92bf6a10004d9a
bendzamba <bendzamba@gmail.com>
2024-10-16 23:53:04 -0400

    detecting we are running in lambda more holistically, using built in environment variable. environment variable change to be a bit more generic. emulating an `event` object structure that API Gateway would send, so that our app behaves the same as if we were intercepting an actual API call

--- backend/app/services/book_search.py
+++ backend/app/services/book_search.py
@@ -10,30 +10,53 @@ if env_path.exists():
     # Load environment variables from a .env file
     load_dotenv(dotenv_path=env_path)
 
-USE_LAMBDA = os.getenv('USE_LAMBDA', 'false').lower() == 'true'
-
 class BookSearch:
-  def __init__(self, use_lambda: bool):
-    self.use_lambda = use_lambda
-      
+  def __init__(self):
+    pass 
+
   async def search_by_title(self, title: str):
-    if self.use_lambda:
-      openlibrary_lambda_function = os.getenv('OPENLIBRARY_LAMBDA_FUNCTION_NAME')
+    # If we are in Lambda, we are behind a VPC, so we need to invoke our separate
+    # Lambda running outside of the VPC
+    if self.is_running_in_lambda():
+      openlibrary_lambda_function = os.getenv('NON_VPC_LAMBDA_FUNCTION_NAME')
       return self.call_lambda(
         title=title,
         openlibrary_lambda_function=openlibrary_lambda_function
       )
-    else:
-      # Call the third-party API directly (for local dev or Docker)
-      return await self.call_direct(title=title)
+    
+    # Not running in Lambda, use local call
+    return await self.call_direct(title=title)
       
+  def is_running_in_lambda():
+    return os.getenv('AWS_EXECUTION_ENV') is not None
+  
   def call_lambda(self, title: str, openlibrary_lambda_function: str):
-      # Invoke our separate Lambda function
+      # Here we emulate an API Gateway call to our Lambda to search for the book
       lambda_client = boto3.client('lambda')
+      event_payload = {
+        "httpMethod": "GET",
+        "path": f"/books/search/{title}",
+        "headers": {
+            "accept": "*/*",
+            "User-Agent": "Mozilla/5.0 ..."
+        },
+        "queryStringParameters": None,
+        "body": None,
+        "isBase64Encoded": False,
+        "requestContext": {
+            "resourcePath": f"/books/search/{title}",
+            "httpMethod": "GET",
+            "domainName": "api.myalexandria.ai",
+            "requestId": "unique-request-id" # Uniqueness is not a requirement
+        },
+        "pathParameters": {
+            "proxy": "books"
+        }
+      }
       response = lambda_client.invoke(
         FunctionName=openlibrary_lambda_function,
         InvocationType="RequestResponse",
-        Payload=json.dumps({"title": title})
+        Payload=json.dumps(event_payload)
       )
       payload = response["Payload"].read()
       return json.loads(payload)
@@ -44,9 +67,7 @@ class BookSearch:
 
 
 def get_book_search():
-  book_search = BookSearch(
-    use_lambda=USE_LAMBDA
-  )
+  book_search = BookSearch()
   try:
     yield book_search
   finally:
