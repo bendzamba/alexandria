744e709a441db9d858d2f7eebe5e76c16f50cb1c
bendzamba <bendzamba@gmail.com>
2024-10-20 23:51:14 -0400

    splitting up open library into local and remote. if we are running in lambda, we want to hit our remote microservice for internet access as we can't access the internet in our VPC without a NAT gateway. if we are NOT in lambda, business as usual

---
+++ backend/app/services/open_library/remote.py
@@ -0,0 +1,47 @@
+import json
+import os
+import boto3
+import inspect
+from app.services.open_library.base import BaseOpenLibraryHandler
+from app.models.exception import ExceptionHandler
+from app.models.openlibrary import Works
+
+
+class RemoteOpenLibraryHandler(BaseOpenLibraryHandler):
+
+  def __init__(self):
+    self.openlibrary_lambda_function = os.getenv('NON_VPC_LAMBDA_FUNCTION_NAME')
+    self.lambda_client = boto3.client('lambda')
+
+  async def search_by_title(self, title: str) -> Works | ExceptionHandler:
+    
+    event_payload = {
+      "method": inspect.currentframe().f_code.co_name,
+      "arguments": {
+        "title": title
+      }
+    }
+
+    return self._invoke_lambda(event_payload=event_payload)
+
+  async def fetch_image_from_olid(self, olid: str | None) -> str:
+
+    event_payload = {
+      "method": inspect.currentframe().f_code.co_name,
+      "arguments": {
+        "olid": olid
+      }
+    }
+
+    return self._invoke_lambda(event_payload=event_payload)
+  
+  async def _invoke_lambda(self, event_payload: dict):
+
+    response = self.lambda_client.invoke(
+      FunctionName=self.openlibrary_lambda_function,
+      InvocationType="RequestResponse",
+      Payload=json.dumps(event_payload)
+    )
+    payload = response["Payload"].read()
+    
+    return json.loads(payload)
