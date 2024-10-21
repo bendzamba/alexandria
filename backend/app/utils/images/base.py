4d0e9c791dac97b8d3783a7c3a6e65b2872aa995
bendzamba <bendzamba@gmail.com>
2024-10-20 23:50:16 -0400

    changing to abstract methods to enforce usage

--- backend/app/utils/images/base.py
+++ backend/app/utils/images/base.py
@@ -1,17 +1,22 @@
 import os
+from abc import ABC, abstractmethod
 
-class BaseImageHandler:
+class BaseImageHandler(ABC):
+    
     def __init__(self):
         self.storage_backend = os.getenv('STORAGE_BACKEND', 'local')
     
+    @abstractmethod
     def download_image(self, url, image_name):
         """To be implemented by subclass."""
-        raise NotImplementedError("This method should be implemented by subclasses.")
+        pass
     
+    @abstractmethod
     def save_image(self, image_name, image_content):
         """To be implemented by subclass."""
-        raise NotImplementedError("This method should be implemented by subclasses.")
-    
+        pass
+
+    @abstractmethod
     def get_image_uri(self, olid):
         """To be implemented by subclass."""
-        raise NotImplementedError("This method should be implemented by subclasses.")
+        pass
