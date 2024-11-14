d1efba6b7755893b198985c28f136d5ac74a629e
bendzamba <bendzamba@gmail.com>
2024-11-14 13:21:21 -0500

    re-working Book model to account for DB changes and change to public API. this is honestly still not ideal, in that for `BookCreate` we still potentially rely on an `olid` when I feel this should likely become part of a separate `Image` object. we 'rebuild' the BookPublic similarly to how we previously rebuilt the BookPublicWithBookshelves to account for the "Image" type, which we can't import prior to this because of circular dependency. ensuring we call image.model_dump() in BookPublic model_dump() as we need to build the URI

--- backend/app/models/book.py
+++ backend/app/models/book.py
@@ -2,9 +2,6 @@ from app.models.book_bookshelf import BookBookshelfLink
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, TYPE_CHECKING, List
 from enum import Enum
-from app.utils.images.factory import get_image_handler
-
-image_handler = get_image_handler()
 
 if TYPE_CHECKING:
     from app.models.bookshelf import Bookshelf  # Only imported when type checking
@@ -19,7 +16,6 @@ class BookBase(SQLModel):
     title: str
     author: str
     year: int
-    olid: Optional[str] = None
     # Instead of creating a new table to host unused OLIDs per book
     # we store the full bore of them as a json-encoded list
     olids: Optional[str] = None
@@ -37,21 +33,28 @@ class Book(BookBase, table=True):
         link_model=BookBookshelfLink,
         sa_relationship_kwargs=dict(lazy="selectin"),
     )
+    image: "Image" = Relationship(
+        back_populates="book",
+        cascade_delete=True,
+        # sa_relationship_kwargs=dict(lazy="selectin"), 
+    )
 
 
 class BookCreate(BookBase):
-    pass
+    olid: Optional[str] = None
 
 
 class BookPublic(BookBase):
     id: int
-    cover_uri: str | None = None
+    image: Optional["ImagePublic"] = None
 
     def model_dump(self, **kwargs):
         data = super().model_dump(**kwargs)
-        if self.olid is not None:
-            data["cover_uri"] = image_handler.get_image_uri(self.olid)
 
+        # Ensure image gets its own `model_dump` call if it's not None
+        if self.image is not None:
+            data["image"] = self.image.model_dump()
+        
         return data
 
 
@@ -59,12 +62,13 @@ class BookUpdate(SQLModel):
     title: Optional[str] = None
     author: Optional[str] = None
     year: Optional[int] = None
-    olid: Optional[str] = None
+    # olid: Optional[str] = None
     rating: Optional[int] = None
     review: Optional[str] = None
     read_status: Optional[ReadStatus] = None
     read_start_date: Optional[str] = None
     read_end_date: Optional[str] = None
+    olid: Optional[str] = None
 
 
 class BookPublicWithBookshelves(BookPublic):
@@ -73,10 +77,11 @@ class BookPublicWithBookshelves(BookPublic):
 
 # `noqa` is used to suppress linter errors
 # we needed the import here to avoid circular imports
+from app.models.image import Image, ImagePublic # noqa
 from app.models.bookshelf import BookshelfPublic  # noqa
 
+BookPublic.model_rebuild()
 BookPublicWithBookshelves.model_rebuild()
-
 
 class BookIds(SQLModel):
     book_ids: List[int]
