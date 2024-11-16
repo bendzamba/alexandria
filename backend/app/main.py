d1139950882eda0e40b1a1ab2baa605096861ea2
bendzamba <bendzamba@gmail.com>
2024-11-16 17:46:37 -0500

    adding middleware to our app to normalize incoming data from form data to json, so our routes only need to handle json data.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
 from contextlib import asynccontextmanager
 from app.routers import books, bookshelves
 from sqlmodel import SQLModel, text
@@ -32,6 +33,27 @@ async def lifespan(app: FastAPI):
     yield
 
 
+# Normalize all incoming data to JSON so as not to burden our routes with
+# validating and handling multiple content types
+# This comes into play when the client sends Form Data along with a file upload
+class FormToJSONMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        content_type = request.headers.get("Content-Type", "").split(";", 1)[0]
+        if content_type == "multipart/form-data":
+            form_data = await request.form()
+            # This can include a File of type UploadFile
+            json_data = None
+            for key, value in form_data.items():
+                if isinstance(value, UploadFile):
+                    json_data[key] = await value.read()
+                else:
+                    json_data[key] = value
+            request.headers.__delitem__("content-type")
+            request.headers["Content-Type"] = "application/json"
+            request._json = json_data
+        return await call_next(request)
+
+
 app = FastAPI(lifespan=lifespan)
 
 if os.getenv("STORAGE_BACKEND") == "local":
@@ -46,6 +68,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(FormToJSONMiddleware)
 
 app.include_router(books.router, prefix="/books", tags=["books"])
 app.include_router(bookshelves.router, prefix="/bookshelves", tags=["bookshelves"])
