ee32ec4cd11e41e0ae66cb127711770949cb07f4
bendzamba <bendzamba@gmail.com>
2024-11-19 00:15:19 -0500

    adding tests for form data and image uploads, to create and patch endpoints

--- backend/app/tests/integration/test_book_routes.py
+++ backend/app/tests/integration/test_book_routes.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import pytest
 import random
 from unittest.mock import patch
@@ -83,7 +84,7 @@ def test_get_book_not_exists(client: TestClient):
     assert response.status_code == 404
 
 
-def test_create_book_correct(client: TestClient):
+def test_create_book_correct_olid(client: TestClient):
     body = {
         "title": "Book Title",
         "author": "Book Author",
@@ -92,6 +93,27 @@ def test_create_book_correct(client: TestClient):
         "read_status": random.choice([e.value for e in ReadStatus])
     }
     response = client.post("/books", json=body)
+    assert response.status_code == 201
+    assert response.json() is None
+
+
+def test_create_book_correct_upload_image_file(client: TestClient):
+    form_data = {
+        "title": "Book Title",
+        "author": "Book Author",
+        "year": 2000,
+        "olid": "abcde",
+        "read_status": random.choice([e.value for e in ReadStatus])
+    }
+    # Simulate an image file upload
+    image_file = ("cover.jpg", BytesIO(b"fake image content"), "image/jpeg")
+    
+    # Send the POST request with multipart/form-data
+    response = client.post(
+        "/books",
+        files={"file": image_file},
+        data=form_data,
+    )
     assert response.status_code == 201
     assert response.json() is None
 
@@ -105,6 +127,42 @@ def test_create_book_incorrect(client: TestClient):
         "bread_status": random.choice([e.value for e in ReadStatus])
     }
     response = client.post("/books", json=body)
+    assert response.status_code == 422
+
+
+def test_create_book_incorrect_upload_bad_form_data(client: TestClient):
+    form_data = {
+        "tidal": "Book Title",
+        "other": "Book Author",
+        "hear": 2000,
+        "ovid": "abcde",
+        "bread_status": random.choice([e.value for e in ReadStatus])
+    }
+    # Simulate an image file upload
+    image_file = ("cover.jpg", BytesIO(b"fake image content"), "image/jpeg")
+    
+    # Send the POST request with multipart/form-data
+    response = client.post(
+        "/books",
+        files={"file": image_file},
+        data=form_data,
+    )
+    assert response.status_code == 422
+
+
+def test_create_book_incorrect_upload_no_image_file(client: TestClient):
+    form_data = {
+        "title": "Book Title",
+        "author": "Book Author",
+        "year": 2000,
+        "olid": "abcde",
+        "read_status": random.choice([e.value for e in ReadStatus])
+    }    
+    # Send the POST request with multipart/form-data
+    response = client.post(
+        "/books",
+        data=form_data,
+    )
     assert response.status_code == 422
 
 
@@ -132,6 +190,24 @@ def test_patch_book_not_exists(client: TestClient, seed_book, seed_image):
     body = {"title": "The Worst American Novel", "year": 2024}
     response = client.patch("/books/12345", json=body)
     assert response.status_code == 404
+
+
+def test_patch_book_with_image_file(client: TestClient, seed_book, seed_image, create_book, create_image):
+    with patch("app.models.image.image_handler") as mock_image_handler:
+        mock_s3_uri = "https://mock-s3-bucket.s3.amazonaws.com/abcde.jpg"
+        mock_image_handler.get_image_uri.return_value = mock_s3_uri
+
+        # Simulate an image file upload
+        image_file = ("cover.jpg", BytesIO(b"fake image content"), "image/jpeg")
+        form_data = {"title": "The Worst American Novel", "year": 2024}
+
+        response = client.patch(
+            f"/books/{create_book.id}",
+            files={"file": image_file},
+            data=form_data,
+        )
+        
+        assert response.status_code == 204
 
 
 def test_delete_book(client: TestClient, seed_book, create_book):
