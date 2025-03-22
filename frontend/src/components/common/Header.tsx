e398dab2a7e2ffc3b0a8d04ab75772a3f8fd3199
bendzamba <bendzamba@gmail.com>
2025-03-21 23:40:34 -0400

    Preventing unnecessary reload if we are already on the page in question

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,15 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import { Navbar } from "react-bootstrap";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
+  const location = useLocation();
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand>
-          <Link className="navbar-brand" to="/">
+          <Link
+            className="navbar-brand"
+            to="/"
+            onClick={(e) => {
+              if (location.pathname === "/") {
+                e.preventDefault(); // Prevent navigation if already on the same page
+              }
+            }}
+          >
             <h1
               className="display-4 mb-0"
               style={{ wordBreak: "break-word", whiteSpace: "normal" }}
@@ -27,8 +36,21 @@ function Header() {
               id="bookshelves-nav-dropdown"
               className="ms-auto"
             >
-              <NavDropdown.Item href="/">View</NavDropdown.Item>
-              <NavDropdown.Item href="/bookshelves/create/">
+              <NavDropdown.Item
+                as={Link}
+                to="/"
+                onClick={(e) => location.pathname === "/" && e.preventDefault()}
+              >
+                View
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                as={Link}
+                to="/bookshelves/create/"
+                onClick={(e) =>
+                  location.pathname === "/bookshelves/create/" &&
+                  e.preventDefault()
+                }
+              >
                 Create
               </NavDropdown.Item>
             </NavDropdown>
@@ -37,8 +59,22 @@ function Header() {
               id="books-nav-dropdown"
               className="ms-auto"
             >
-              <NavDropdown.Item href="/books/">View</NavDropdown.Item>
-              <NavDropdown.Item href="/books/create/">Create</NavDropdown.Item>
+              <NavDropdown.Item
+                as={Link}
+                to="/books/"
+                onClick={(e) => location.pathname === "/" && e.preventDefault()}
+              >
+                View
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                as={Link}
+                to="/books/create/"
+                onClick={(e) =>
+                  location.pathname === "/books/create/" && e.preventDefault()
+                }
+              >
+                Create
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
