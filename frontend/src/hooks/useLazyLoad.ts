63f84d6f27f19086da0e9f8158dde1fc30b64d23
bendzamba <bendzamba@gmail.com>
2025-03-24 12:31:18 -0400

    Adding custom hook to observe all images in a given root element, so we are not creating individual observers per lazy-loading image

---
+++ frontend/src/hooks/useLazyLoad.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState, useRef } from "react";
+
+const useLazyLoad = (rootRef: React.RefObject<HTMLElement>) => {
+  const [visibleImages, setVisibleImages] = useState<Set<string>>(new Set());
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  if (!rootRef) {
+    console.log("Could not find root element");
+  }
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver(
+      (entries) => {
+        setVisibleImages((prev) => {
+          const updated = new Set(prev);
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              const target = entry.target as HTMLElement;
+              updated.add(target.dataset.src as string);
+            }
+          });
+          return updated;
+        });
+      },
+      { root: rootRef.current, threshold: 0.1, rootMargin: "0px 0px 50px 0px" }
+    );
+
+    return () => observerRef.current?.disconnect();
+  }, []);
+
+  return {
+    observe: (el: Element | null) => el && observerRef.current?.observe(el),
+    visibleImages,
+  };
+};
+
+export default useLazyLoad;
