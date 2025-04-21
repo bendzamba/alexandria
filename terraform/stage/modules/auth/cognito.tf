75ecb30283cd6f1271aa58f375801a80a7d75cce
bendzamba <bendzamba@gmail.com>
2025-04-21 14:44:04 -0400

    Adding cognito config based on terraformer and manual changes, matching setup initialized using AWS console

--- terraform/stage/modules/auth/cognito.tf
+++ terraform/stage/modules/auth/cognito.tf
@@ -1,22 +1,24 @@
 resource "aws_cognito_user_pool" "cognito_user_pool" {
-  name = var.app_name
-
-  username_attributes = ["email"]  # Users sign in with email
-
-  alias_attributes = ["preferred_username"]  # Allows an alias like a display name
-
-  deletion_protection = "ACTIVE"
+  name                      = "alexandria"
+  auto_verified_attributes  = ["email"]
+  deletion_protection       = "ACTIVE"
+  mfa_configuration         = "OFF"
+  username_attributes       = ["email"]
 
   account_recovery_setting {
     recovery_mechanism {
-      name = "verified_email"
-      priority = 1
+      name     = "verified_email"
+      priority = "1"
+    }
+
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = "2"
     }
   }
 
-  device_configuration {
-    challenge_required_on_new_device = true
-    device_only_remembered_on_user_prompt = true
+  admin_create_user_config {
+    allow_admin_create_user_only = "false"
   }
 
   email_configuration {
@@ -24,66 +26,89 @@ resource "aws_cognito_user_pool" "cognito_user_pool" {
   }
 
   password_policy {
-    minimum_length    = 8
-    require_lowercase = true
-    require_numbers   = true
-    require_symbols   = true
-    require_uppercase = true
-    temporary_password_validity_days = 30
+    minimum_length                   = "8"
+    password_history_size            = "0"
+    require_lowercase                = "true"
+    require_numbers                  = "true"
+    require_symbols                  = "true"
+    require_uppercase                = "true"
+    temporary_password_validity_days = "7"
   }
 
   schema {
-    attribute_data_type = "String"
-    name                = "given_name"
-    required            = false
-    mutable             = true
+    attribute_data_type      = "String"
+    developer_only_attribute = "false"
+    mutable                  = "true"
+    name                     = "family_name"
+    required                 = "true"
+
     string_attribute_constraints {
-      min_length = 0
-      max_length = 100
+      max_length = "2048"
+      min_length = "0"
     }
   }
 
   schema {
-    attribute_data_type = "String"
-    name                = "family_name"
-    required            = false
-    mutable             = true
+    attribute_data_type      = "String"
+    developer_only_attribute = "false"
+    mutable                  = "true"
+    name                     = "given_name"
+    required                 = "true"
+
     string_attribute_constraints {
-      min_length = 0
-      max_length = 100
+      max_length = "2048"
+      min_length = "0"
     }
   }
 
   username_configuration {
-    case_sensitive = false
+    case_sensitive = "false"
   }
 
-  admin_create_user_config {
-    allow_admin_create_user_only = true
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
   }
 }
 
 resource "aws_cognito_user_pool_client" "cognito_user_pool_client" {
-  name         = "${var.app_name}-client"
-  user_pool_id = aws_cognito_user_pool.cognito_user_pool.id
+  name                                          = "${var.app_name}-client"
+  user_pool_id                                  = aws_cognito_user_pool.cognito_user_pool.id
+  access_token_validity                         = "60"
+  allowed_oauth_flows                           = ["code"]
+  allowed_oauth_flows_user_pool_client          = "true"
+  allowed_oauth_scopes                          = ["email", "openid", "profile"]
+  auth_session_validity                         = "3"
+  callback_urls                                 = ["http://localhost:3000", "https://${var.app_domain}"]
+  enable_propagate_additional_user_context_data = "false"
+  enable_token_revocation                       = "true"
+  explicit_auth_flows                           = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH"]
+  id_token_validity                             = "60"
+  logout_urls                                   = ["http://localhost:3000/logout", "https://${var.app_domain}/logout"]
+  prevent_user_existence_errors                 = "ENABLED"
+  refresh_token_validity                        = "5"
+  supported_identity_providers                  = ["COGNITO"]
 
-  explicit_auth_flows = [
-    "ALLOW_USER_SRP_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH"
-  ]
+  token_validity_units {
+    access_token  = "minutes"
+    id_token      = "minutes"
+    refresh_token = "days"
+  }
+}
 
-  supported_identity_providers = ["COGNITO"]
+resource "aws_cognito_user_pool_domain" "auth_domain" {
+  domain          = "auth.${var.app_domain}"
+  certificate_arn = var.certificate_arn
+  user_pool_id    = aws_cognito_user_pool.cognito_user_pool.id
+}
 
-  prevent_user_existence_errors = "ENABLED"
+resource "aws_route53_record" "auth_a_record" {
+  name    = "auth.${var.app_domain}"
+  type    = "A"
+  zone_id = var.route53_zone_id
 
-  allowed_oauth_flows = ["implicit"]  # Important for SPAs
-  allowed_oauth_scopes = [
-    "email",
-    "openid",
-    "profile"
-  ]
-
-  allowed_oauth_flows_user_pool_client = true
-  callback_urls = ["https://${var.app_domain}"]  # Return URL
-  logout_urls   = ["https://${var.app_domain}/logout"]
+  alias {
+    name                   = aws_cognito_user_pool_domain.auth_domain.cloudfront_distribution
+    zone_id                = aws_cognito_user_pool_domain.auth_domain.cloudfront_distribution_zone_id
+    evaluate_target_health = false
+  }
 }
--- terraform/stage/modules/auth/variables.tf
+++ terraform/stage/modules/auth/variables.tf
@@ -5,3 +5,11 @@ variable "app_name" {
 variable "app_domain" {
   type = string
 }
+
+variable "certificate_arn" {
+  type = string
+}
+
+variable "route53_zone_id" {
+  type = string
+}
